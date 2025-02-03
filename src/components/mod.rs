772d410f4850efa8c2a76c71f343f5e1abfab957
mahdi739 <86552031+mahdi739@users.noreply.github.com>
2025-02-03 11:02:12 +0000

    Deploying to gh-pages from @ mahdi739/tauri_gardesh@20dc824b724a718155cf037927616fbb35a7bdc8 🚀

--- src/components/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod app;
-pub mod place_card;
-pub mod session;
-pub mod suggesions;
-pub mod suggestion_item;
-pub mod sidebar;
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use leptos::prelude::*;
-use tauri_gardesh_ui::components::app::App;
-
-fn main() {
-  console_error_panic_hook::set_once();
-  mount_to_body(App)
-}
